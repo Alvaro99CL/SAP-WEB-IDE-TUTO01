f277aec2c44cf4b650e8ff06cca5c528233b63c3
Alvaro Gonzalez <agonzalezcl@gmail.com>
2020-08-24 15:57:56 +0000

    commit unidad 2-1

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	'sap/ui/core/mvc/Controller',
+	'sap/m/MessageToast',
+	'opensap/myapp/model/formatter'
+	],
+	function(Controller , MessageToast , formatter) {
+	"use strict";
+	
+	Controller.extend("opensap.myapp.controller.App",{
+		
+		formatter : formatter,
+		
+		onShowHola : function(){
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+
+		}
+	});
+
+});
